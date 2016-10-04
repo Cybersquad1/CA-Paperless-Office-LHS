97209475ce4f2a96fb386b8cc3127bf5a0779b17
Sverre Oste <sverre.oste@gmail.com>
2016-10-04 15:30:44 +0200

    Publish static css,js and html files

--- src/website/Server.js
+++ src/website/Server.js
@@ -1,10 +1,33 @@
+var fs = require("fs");
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile(__dirname + "/" + "index.html");
 });
 
-app.listen(3000, function () {
+function Log(err) {
+    console.log(err);
+}
+
+function PublishDir(dir) {
+    if (fs.statSync(__dirname + dir).isDirectory()) {
+        app.get(dir + "/*", function (req, res) {
+            if (fs.existsSync(__dirname + req.path)) {
+                res.sendFile(__dirname + req.path);
+            }
+        });
+        return;
+    }
+    Log("ERROR: FAILED!");
+}
+
+PublishDir("/js");
+PublishDir("/css");
+PublishDir("/fonts");
+
+
+
+app.listen(80, function () {
     console.log('Example app listening on port 3000!');
 });
