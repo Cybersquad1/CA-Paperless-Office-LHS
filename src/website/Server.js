93a54588b32ccb630c71094044507362d2673e90
Sverre Oste <sverre.oste@gmail.com>
2016-10-24 09:00:14 +0200

    first try for fileupload

--- src/website/Server.js
+++ src/website/Server.js
@@ -4,6 +4,13 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var app = express();
 
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
+var multiparty = require("multiparty");
+//var azure = require("azure");
+
+
 var UH = require('./UserHandler.js');
 var EE = require('./ErrorEvent.js');
 var UserHandler = new UH(debug);
@@ -117,6 +124,31 @@ UserHandler.Init(app, function (err) {
 
     app.get('/', function (req, res) {
         res.sendFile(__dirname + "/" + "index.html");
+    });
+
+    app.post("/upload"/*, multipartMiddleware*/, function (req, res) {
+        //console.log(req.body, req.files);
+
+        //var blobService = azure.createBlobService();
+        var form = new multiparty.Form();
+        form.on('part', function (part) {
+            if (!part.filename) return;
+
+            var size = part.byteCount;
+            var name = part.filename;
+            console.log("size:"+ size +" Filename:" +name);
+            //console.log(part);
+            //var container = 'blobContainerName';
+
+            /*blobService.createBlockBlobFromStream(container, name, part, size, function (error) {
+                if (error) {
+                    // error handling
+                }*/
+        });
+        form.parse(req);
+
+        res.send('File uploaded successfully');
+
     });
 
     PublishDir("/js");
