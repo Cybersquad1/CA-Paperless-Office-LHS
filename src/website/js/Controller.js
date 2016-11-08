53abc547481e015d8c4cf77d08087d51207d3ed4
Levi Gillis <gillislevi@gmail.com>
2016-11-08 11:47:21 +0100

    added client side hashing of password

--- src/website/js/Controller.js
+++ src/website/js/Controller.js
@@ -4,33 +4,37 @@
 var app = angular.module('myApp', ['angularFileUpload']);
 app.controller('PaperlessController', function ($scope, $http) {
     $scope.login = function () {
+        var pass = CryptoJS.SHA512($scope.loginPassword).toString();
+
         $scope.loginData = {
-            username: $scope.loginUsername,
-            password: $scope.loginPassword
+            "username": $scope.loginUsername,
+            "password": pass
         };
 
         var logindata = JSON.stringify($scope.loginData);
 
-        $http.post('/login', logindata).then(function succesCallback(logindata) {
-            console.log(logindata);
+        $http.post('/login', logindata).then(function(logindatares) {
+            console.log(logindatares);
         });
 
         console.log('User logged in', logindata);
-    }
+    };
 
     $scope.register = function () {
+        var pass = CryptoJS.SHA512($scope.registerPassword).toString();
+
         $scope.registerData = {
-            username: $scope.registerUsername,
-            password: $scope.registerPassword,
-            email: $scope.registerEmail
+            "username": $scope.registerUsername,
+            "password": pass,
+            "email": $scope.registerEmail
         };
 
         var registerdata = JSON.stringify($scope.registerData);
 
-        $http.post('/register', registerdata).then(function succesCallback(registerdata) {
-            console.log(registerdata);
+        $http.post('/register', registerdata).then(function(registerdatares) {
+            console.log(registerdatares);
         });
 
         console.log('User registered', registerdata);
-    }
+    };
 });
